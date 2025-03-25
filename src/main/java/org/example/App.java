eb726403c212eb66d9d68cc36db8828aa98f362c
Yullc <apanan0910@gmail.com>
2025-03-25 11:22:38 +0900

    AM 날짜가져오기

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,17 +1,17 @@
-package org.example;
-
-public class App {
-    ArticleController articleController = new ArticleController();
-    public void run() {
-        while(true) {
-            String cmd = Container.getScanner().nextLine().trim();
-            if(cmd.equals("article write")){
-                articleController.write();
-            }
-            else if(cmd.equals("article list")){
-                articleController.list();
-            }
-        }
-
-    }
-}
+//package org.example;
+//
+//public class App {
+//    ArticleController articleController = new ArticleController();
+//    public void run() {
+//        while(true) {
+//            String cmd = Container.getScanner().nextLine().trim();
+//            if(cmd.equals("article write")){
+//                articleController.write();
+//            }
+//            else if(cmd.equals("article list")){
+//                articleController.list();
+//            }
+//        }
+//
+//    }
+//}
--- src/main/java/org/example/Article.java
+++ src/main/java/org/example/Article.java
@@ -1,46 +1,47 @@
-package org.example;
-
-public class Article {
-    private int id;
-    private String body;
-    private String title;
-
-    public Article(int id, String body, String title) {
-        this.id = id;
-        this.body = body;
-        this.title = title;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitleTitle(String title) {
-        this.title = title;
-    }
-
-    @Override
-    public String toString() {
-        return "Motivation{" +
-                "id=" + id +
-                ", body='" + body + '\'' +
-                ", title='" + title + '\'' +
-                '}';
-    }
-}
+//package org.example;
+//
+//class Article {
+//    private int id;
+//    private String title;
+//    private String body;
+//    private String nowtime;
+//
+//    public Article(int id, String title, String body, String nowtime) {
+//        this.id = id;
+//        this.title = title;
+//        this.body = body;
+//        this.nowtime = nowtime;
+//    }
+//
+//    public String getNowtime() {
+//        return nowtime;
+//    }
+//
+//    public void setNowtime() {
+//        this.nowtime = nowtime;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getBody() {
+//        return body;
+//    }
+//
+//    public void setBody(String body) {
+//        this.body = body;
+//    }
+//}
