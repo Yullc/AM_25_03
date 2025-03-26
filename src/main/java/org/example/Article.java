9422e23f698fc9c2ebe21a42e764ed3fc2767cf5
Yullc <apanan0910@gmail.com>
2025-03-26 10:24:49 +0900

    회원 테스트 데이터 추가, 리팩토링

--- src/main/java/org/example/Article.java
+++ src/main/java/org/example/Article.java
@@ -1,47 +1,57 @@
-//package org.example;
-//
-//class Article {
-//    private int id;
-//    private String title;
-//    private String body;
-//    private String nowtime;
-//
-//    public Article(int id, String title, String body, String nowtime) {
-//        this.id = id;
-//        this.title = title;
-//        this.body = body;
-//        this.nowtime = nowtime;
-//    }
-//
-//    public String getNowtime() {
-//        return nowtime;
-//    }
-//
-//    public void setNowtime() {
-//        this.nowtime = nowtime;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//
-//    public String getBody() {
-//        return body;
-//    }
-//
-//    public void setBody(String body) {
-//        this.body = body;
-//    }
-//}
+package org.example;
+
+public class Article {
+    private int id;
+    private String regDate;
+    private String updateDate;
+    private String title;
+    private String body;
+
+    public Article(int id, String regDate, String updateDate, String title, String body) {
+        this.id = id;
+        this.regDate = regDate;
+        this.updateDate = updateDate;
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
